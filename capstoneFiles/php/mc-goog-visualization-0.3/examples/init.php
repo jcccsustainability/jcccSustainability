41b25fc388a1ddbb640588b7f43bebc5be1700ab
Michael.Read <ww2airforce@gmail.com>
2013-02-22 23:53:11 -0600

    deleted google chart helper api files (they didnt help)

--- capstoneFiles/php/mc-goog-visualization-0.3/examples/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('include_path', realpath(dirname(__FILE__) . '/../lib/'));
-
-require_once 'MC/Google/Visualization.php';
-
-$db = new PDO('sqlite:example.db');
-$vis = new MC_Google_Visualization($db);
-?>
